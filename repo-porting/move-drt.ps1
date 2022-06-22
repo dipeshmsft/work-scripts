284fa469755b6e167ebab6f5a8ad3f1832cb0505
Dipesh Kumar <dipeshkumar@microsoft.com>
2022-06-22 15:47:51 +0530

    Made some changes to move-drt

---
+++ repo-porting/move-drt.ps1
@@ -0,0 +1,53 @@
+[CmdletBinding(PositionalBinding=$false)]
+Param(
+    [string][Alias('b')]$branchName,
+    [string][Alias('t')]$relativeTestDirPath,
+    [string][Alias('r')]$repoPortingDir
+)
+
+$cleanDir = "D:\repos\public-test\clean\";
+$publicDir = "D:\repos\public-test\";
+$intDir = "D:\repos\int-test\";
+
+$sourcePath = Join-Path $intDir $relativeTestDirPath;
+$destinationPath = Join-Path $publicDir $relativeTestDirPath;
+$cleanPath = Join-Path $cleanDir $branchName;
+
+Write-Output $relativeTestDirPath, $destinationPath, $cleanPath;
+
+# Process
+Set-Location $publicDir;
+git checkout main;
+# git pull origin main;
+git branch $branchName;
+git checkout $branchName;
+Write-Output("Branch $branchName created");
+
+xcopy.exe $sourcePath $destinationPath /S;
+Write-Output( "File copied to public directory ");
+
+$commitMsg = -Join ("Added ", $branchName.replace('-',' ').replace('_', ' '));
+git stage --all;
+git reset ".\move-drt.ps1";
+git commit -m $commitMsg;
+Write-Output( "Initial commit of files done ");
+
+$sanitizeToolDir = Join-Path $repoPortingDir "Sanitize";
+Write-Output("In $sanitizeToolDir");
+
+Set-Location $sanitizeToolDir;
+
+$trimmedDestinationPath = $destinationPath.TrimEnd('\');
+$trimmedCleanPath = $cleanPath.TrimEnd('\');
+.\sanitize.ps1 $trimmedDestinationPath $trimmedCleanPath;
+
+# $codeFormattingDir = Join-Path $repoPortingDir "CodeFormatter";
+# $formattingProjFile = Join-Path $codeFormattingDir "CodeFormatting.csproj";
+
+# Set-Location $codeFormattingDir;
+# .\CodeFormatter.exe  $formattingProjFile /verbose /rule-:BraceNewLine,NewLineAbove,UnicodeLiterals,UsingLocation,ExplicitThis,ExplicitVisibility,FormatDocument,ReadonlyFields,FieldNames,CustomCopyright /copyright+;
+
+Remove-Item $destinationPath -Recurse;
+xcopy.exe $cleanPath $destinationPath /S;
+Remove-Item $cleanPath -Recurse
+Set-Location $publicDir;
